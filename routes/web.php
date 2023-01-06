017c7209f4fcf9cdcd0841028b55d3477cc07ed8
PutriKusuma90 <94095118+PutriKusuma90@users.noreply.github.com>
2023-01-06 14:42:03 +0700

    Jawaban Soal 1

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\DosenController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::group dosen
+Route::group(['prefix' => '/dosen'], function() {
+    Route::get('/profil', [DosenController::class, 'profil'])->name('dosen.profil');
+    Route::get('/ampu', [DosenController::class, 'ampu'])->name('dosen.dataampu');
+});
